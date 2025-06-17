d8b0c3f8218e112f7fa86092699414af447b1625
victorAMSP <victor.a.p@hotmail.com>
2025-06-17 10:05:28 -0300

    Atualiza .gitignore e remove arquivos desnecessários do commit

--- application/use_cases/apagar_habito_por_id.py
+++
@@ -1,8 +0,0 @@
-from domain.repositories.habito_repository import IHabitorepository
-
-class ApagarHabitoPorIdUseCase:
-    def __init__(self, habito_repository: IHabitorepository):
-        self.habito_repository = habito_repository
-
-    def executar(self, habito_id: int):
-        self.habito_repository.apagar_por_id(habito_id)
--- application/use_cases/listar_habitos_usecase.py
+++
@@ -1,9 +0,0 @@
-from domain.repositories.habito_repository import IHabitorepository
-
-class ListarHabitosComIdUseCase:
-    def __init__(self, habito_repository: IHabitorepository):
-        self.habito_repository = habito_repository
-
-    def executar(self, usuario: str):
-        return self.habito_repository.listar_com_id(usuario)
-    
